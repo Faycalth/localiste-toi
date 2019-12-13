f2538cdfb8ddc20840fd7605eb5a41c52aa5a655
Faycalth <faycalthamri38@gmail.com>
2019-12-13 16:55:02 +0100

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { geolocated } from "react-geolocated";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("https://download.data.grandlyon.com/ws/rdata/sit_sitra.sittourisme/all.json?maxfeatures=100&start=1")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          console.log(result['values'])
+          this.setState({
+            isLoaded: true,
+            items: result['values']
+          });
+        },
+        // Remarque : il est important de traiter les erreurs ici
+        // au lieu d'utiliser un bloc catch(), pour ne pas passer à la trappe
+        // des exceptions provenant de réels bugs du composant.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    /*
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Erreur : {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Chargement…</div>;
+    } else {
+      return (
+        <ul>
+          {items.map(item => (
+            <li key={item.commune}>
+              {item.commune} : {item.nom} ({item.adresse})  
+            </li>
+          ))}
+        </ul>
+      );
+    }
+}}
+*/
+return !this.props.isGeolocationAvailable ? (
+  <div>Il est impossible de vous géolocaliser</div>
+) : !this.props.isGeolocationEnabled ? (
+  <div>La géolocalisation n'est pas activée</div>
+) : this.props.coords ? (
+      <div><a rel="noopener noreferrer" href={'https://www.google.com/maps?q=loc:'+this.props.coords.latitude+','+this.props.coords.longitude} target="_blank">Clique ici pour connaitre ta localisation</a></div>
+) : (
+  <div>Chargement de votre localisation&hellip; </div>
+);
 }
-
-export default App;
+}
+export default geolocated({
+  positionOptions: {
+      enableHighAccuracy: false,
+  },
+  userDecisionTimeout: 5000,
+})(App);
---
+++ src/Map_component.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
+
+const mapStyles = {
+  width: '100%',
+  height: '100%',
+};
+
+
+const Map_component => () => {return (
+        <Map
+          google={this.props.google}
+          zoom={8}
+          style={mapStyles}
+          initialCenter={{ lat: 47.444, lng: -122.176}}
+        />
+    );
+  }
+}
+export default Map_component
